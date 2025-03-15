d0262d90affd05ceb68672fc0e8ac0ca2658c2c5
wingedx-network <wingedxnetwork@gmail.com>
2025-03-15 13:27:25 +0500

    msg

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const apkUrl = 'https://drive.google.com/uc?export=download&id=1fZc4fjCbicMPFYY_2cPYbwl3QdRxpIIM';
+  // const apkUrl = 'https://drive.google.com/uc?export=download&id=1fZc4fjCbicMPFYY_2cPYbwl3QdRxpIIM';
+  const apkUrl = 'https://www.dropbox.com/scl/fi/5ecg9rprvy1egyt9vtjal/btw.apk?rlkey=9746t6nba7eqmwss8o0ahrbht&st=cl8ie0v4&dl=1';
   res.redirect(apkUrl);
 });
 
