65d66f9630db0ebd487aa45d1c227b6bc784fd38
usman-ali-2000 <usmanzulfiqar14@gmail.com>
2025-03-18 19:24:22 +0500

    Msg

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   // const apkUrl = 'https://drive.google.com/uc?export=download&id=1fZc4fjCbicMPFYY_2cPYbwl3QdRxpIIM';
-  const apkUrl = 'https://www.dropbox.com/scl/fi/hovvoipb7j4begkb37wlx/btw.apk?rlkey=dld7z65kt3wv8im5brh9bpo09&st=epb6ywph&dl=1';
+  const apkUrl = 'https://www.dropbox.com/scl/fi/hovvoipb7j4begkb37wlx/btw.apk?rlkey=dld7z65kt3wv8im5brh9bpo09&st=epb6ywph&raw=1';
   res.redirect(apkUrl);
 });
 
